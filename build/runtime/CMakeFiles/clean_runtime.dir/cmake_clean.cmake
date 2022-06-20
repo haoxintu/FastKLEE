22b1764eec98cc2d356a0037bdfae214ef99c19c
AnonyGiit <haoxintu@gmail.com>
2022-06-20 15:14:48 +0800

    initial version

--- build/runtime/CMakeFiles/clean_runtime.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/clean_runtime"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/clean_runtime.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
